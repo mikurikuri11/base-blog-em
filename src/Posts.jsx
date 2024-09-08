617c78fae422142ed15343d0cc07b084b04fbb6c
mikurikuri11 <mikuriyakenta003@gmail.com>
2024-09-08 14:13:31 +0900

    ✨ tanstack queryを用いた最も簡単なフェッチ

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { fetchPosts, deletePost, updatePost } from "./api";
 import { PostDetail } from "./PostDetail";
@@ -8,8 +9,33 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery({
+    // args1: what defines this data within the query cache, always an array
+    queryKey: ["posts"],
+    // args2: function that's going to run to fetch the data
+    queryFn: fetchPosts,
+  });
+
+  // data is undefined
+  // because fetch posts is asynchronous function that is going to take some amount of time to return
+  // if (!data) {
+  //   return <div></div>;
+  // }
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  // it tries the query function several times before it gives up
+  // something that is configurable
+  if (isError) {
+    return (
+      <>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
+  }
 
   return (
     <>
